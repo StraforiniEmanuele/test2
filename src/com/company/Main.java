359d59d241775dccc816728a5cd70746900d7679
StraforiniEmanuele <straforini.emanuele@gmail.com>
2021-12-01 09:51:41 +0100

    added christmas tree

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,13 +10,13 @@ public class Main {
 
         Calcolo calcolo = new Calcolo();
 
-        /*for (int i = 1; i<10000; i++) {
+        for (int i = 1; i<10000; i++) {
             System.out.println("Numero in questione: " + i);
             calcolo.calcolo(i);
         }
 
         System.out.println(calcolo.toString());
-        calcolo.alberoDiNatale(9);*/
+        calcolo.alberoDiNatale(9);
         List<String> strings = new ArrayList<>();
         strings.add("a");
         strings.add("b");
